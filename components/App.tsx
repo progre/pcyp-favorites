1ffd06b1aae25a49465b79c82277957123cf4120
progre <djyayutto@gmail.com>
2021-01-31 17:57:15 +0900

    feat: add design

--- components/App.tsx
+++ components/App.tsx
@@ -7,6 +7,7 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Typography,
 } from '@material-ui/core';
 import { Container } from '@material-ui/core';
 import { GetApp } from '@material-ui/icons';
@@ -14,30 +15,32 @@ import { DropzoneArea } from 'material-ui-dropzone';
 import React from 'react';
 import Favorite from './Favorite';
 
-export type Props = Readonly<{
+import styles from './App.module.css';
+
+export interface Props {
   favorites: readonly Favorite[];
   onDropFile(file: File): void;
   onClickGetByPeCaRecorder(): string;
-}>;
+}
 
-export default function App(props: Props): JSX.Element {
+function Header(props: Props) {
   return (
-    <Container>
-      <div hidden={props.favorites.length > 0}>
-        <DropzoneArea
-          filesLimit={1}
-          showPreviewsInDropzone={false}
-          showAlerts={['error', 'info']}
-          onChange={(newFiles) => {
-            if (newFiles.length === 0) {
-              return;
-            }
-            props.onDropFile(newFiles[0]);
-          }}
-        />
+    <div className={styles.header}>
+      <div className={styles.description}>
+        <Typography>
+          pcypLite のお気に入りを PeCaRecorder のフィルタ設定に変換します。
+        </Typography>
+        <ol>
+          <li>
+            pcypLite のインストールフォルダにある Favorite.ini
+            をドラッグアンドドロップする
+          </li>
+          <li>変換ボタンをクリックする</li>
+        </ol>
       </div>
-      <div hidden={props.favorites.length === 0}>
+      <div>
         <Button
+          disabled={props.favorites.length === 0}
           variant="contained"
           color="primary"
           startIcon={<GetApp />}
@@ -49,26 +52,54 @@ export default function App(props: Props): JSX.Element {
         >
           変換
         </Button>
-        <TableContainer component={Paper}>
-          <Table size="small" aria-label="a dense table">
-            <TableHead>
-              <TableRow>
-                <TableCell align="right">フィルタ名</TableCell>
-                <TableCell>検索文字列</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {props.favorites.map((row) => (
-                <TableRow key={row.name}>
-                  <TableCell component="th" scope="row" align="right">
-                    {row.name}
-                  </TableCell>
-                  <TableCell>{row.regExp}</TableCell>
+      </div>
+    </div>
+  );
+}
+
+export default function App(props: Props): JSX.Element {
+  return (
+    <Container className={styles.root}>
+      <div className={styles.container}>
+        <Header {...props} />
+        <div
+          className={`${styles.content} ${styles.dropzoneAreaContent}`}
+          hidden={props.favorites.length > 0}
+        >
+          <DropzoneArea
+            filesLimit={1}
+            showPreviewsInDropzone={false}
+            showAlerts={['error', 'info']}
+            onChange={(newFiles) => {
+              if (newFiles.length === 0) {
+                return;
+              }
+              props.onDropFile(newFiles[0]);
+            }}
+          />
+        </div>
+        <div className={styles.content} hidden={props.favorites.length === 0}>
+          <TableContainer component={Paper}>
+            <Table size="small" aria-label="a dense table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="right">フィルタ名</TableCell>
+                  <TableCell>検索文字列</TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableHead>
+              <TableBody>
+                {props.favorites.map((row) => (
+                  <TableRow key={row.name}>
+                    <TableCell component="th" scope="row" align="right">
+                      {row.name}
+                    </TableCell>
+                    <TableCell>{row.regExp}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
       </div>
     </Container>
   );
---
+++ pages/_app.tsx
@@ -0,0 +1,5 @@
+import './globals.css';
+
+export default function MyApp({ Component, pageProps }: any) {
+  return <Component {...pageProps} />;
+}
